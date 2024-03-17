bc7f2bb77fce71a1a08e82ea03663b096e39b9a5
Wu Xiaobo <34577397+JayWu7@users.noreply.github.com>
2024-03-17 01:16:56 +0100

    Add files via upload

---
+++ bp_env.sh
@@ -0,0 +1,20 @@
+sudo apt-get install unzip -y
+unzip bpx-api-py.zip 
+cd bpx-api-py/bpx 
+sudo apt-get install pip -y
+pip install cryptography --break-system-packages 
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <n>"
+    exit 1
+fi
+
+n=$1
+
+nohup python3 bp_bot.py $n > output_$n.log 2>&1 &
+sleep 10
+n=$((n+1))
+nohup python3 bp_bot.py $n > output_$n.log 2>&1 &
+sleep 10
+n=$((n+1))
+nohup python3 bp_bot.py $n > output_$n.log 2>&1 &
